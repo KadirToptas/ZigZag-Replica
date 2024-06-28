ad5b08bd8691ddaa77d2b8da6e44cabb731416fc
KadirToptas <kdrrtptss@gmail.com>
2024-06-28 12:23:37 +0300

    .

---
+++ Assets/Scripts/Ground/GroundPositionController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundPositionController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
